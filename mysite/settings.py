0c78a9cf95db58ddaecf473c315fa4477cec9a2b
Bektur <tbekturtk8@gmail.com>
2022-07-29 19:58:13 +0600

    pagination, comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tbekturtk8@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwerty123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
